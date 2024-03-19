b3ce6f354155d8cda3bb8fe42dfb04476b284e78
Deny Iqbal Mubarok <denyiqbalmubarok@gmail.com>
2024-03-19 22:24:31 +0700

    Finishing Feature Detail Activity (if you want to goto Detail Activity click Profile Button)

---
+++ app/src/main/java/com/example/and_km6_denyiqbalmubarok_challengechapter2/model/MenuDetail.kt
@@ -0,0 +1,16 @@
+package com.example.and_km6_denyiqbalmubarok_challengechapter2.model
+
+import android.os.Parcelable
+import androidx.annotation.DrawableRes
+import kotlinx.parcelize.Parcelize
+import java.util.UUID
+
+@Parcelize
+data class MenuDetail(
+    @DrawableRes
+    var image: Int,
+    var name: String,
+    var desc: String,
+    var shopLoc: String,
+    var price: Double
+): Parcelable
